bd0b149afe02c000b8ef8b61c3d84897ab27204e
Richa-iitr <richa31102@gmail.com>
2024-12-08 04:25:15 +0530

    minor changes

--- post_meme.py
+++ post_meme.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv("API_KEY2")
 API_KEY_SECRET = os.getenv("API_KEY_SECRET2")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN2")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET2")
-BEARER_TOKEN = os.getenv("BEARER_TOKEN2")  # v2 API requires a Bearer Token
+BEARER_TOKEN = os.getenv("BEARER_TOKEN2") 
 
 client = tweepy.Client(bearer_token=BEARER_TOKEN, 
                        consumer_key=API_KEY,
@@ -20,19 +20,10 @@ client = tweepy.Client(bearer_token=BEARER_TOKEN,
                        access_token_secret=ACCESS_TOKEN_SECRET)
 
 def post_tweet_with_online_image(content, image_url):
-    """
-    Post a tweet with an image downloaded from an online URL.
-    
-    Parameters:
-    - content (str): The text content of the tweet.
-    - image_url (str): URL of the image to be downloaded and posted.
-    """
     try:
-        # Download the image from the URL
         response = requests.get(image_url)
         response.raise_for_status()
         
-        # Save the image temporarily in memory
         from io import BytesIO
         image_file = BytesIO(response.content)
         image_file.name = "temp_image.jpg"  
@@ -53,9 +44,8 @@ def post_tweet_with_online_image(content, image_url):
 
 if __name__ == "__main__":
 
-    tweet_content = "Here's a tweet with an image using Tweepy! 🖼️"
     topics = ["supra", "Cross-chain transactions", "speed", "security", "interoperability"]
     audience = "Crypto enthusiasts, developers, blockchain professionals"
     image_path = create_meme(topics, audience)
-    print("Imgage path:", image_path)
-    post_tweet_with_online_image(tweet_content, image_path)
+    print("Image path:", image_path)
+    post_tweet_with_online_image("", image_path)
